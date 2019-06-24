49e2940f1e801fc65b382921a34a07a2e07cf400
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-06-24 15:15:52 -0400

    number could be negative

--- twoSum.js
+++ twoSum.js
@@ -32,7 +32,7 @@ function twoSum(nums, target) {
 // const twoSum = function(nums, target) {
 //     const result = {};
 //     for(let i = 0; i < nums.length; i++) {
-//         if(result[nums[i]] >= 0) {
+//         if(result[nums[i]]) {
 //             return [result[nums[i]] , i]
 //         }
 //         result[target-nums[i]] = i
